cce7d745992d519dcbbaff36c99de29b5ed3243c
Алексей Малыгин <aleks.malygin200@mail.ru>
2026-04-13 09:36:43 +0500

    blogicum 4

--- blogicum/urls.py
+++ blogicum/urls.py
@@ -5,18 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 🔥 ИЗМЕНЕНО: сначала свои пути users, потом стандартные auth
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ]
 
-# 🔥 ДОБАВЛЕНО: обработчики ошибок
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
 
-# 🔥 ДОБАВЛЕНО: для разработки (медиафайлы и debug_toolbar)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
--- pages/views.py
+++ pages/views.py
@@ -11,15 +11,12 @@ class RulesView(TemplateView):
 
 
 def page_not_found(request, exception):
-    """Кастомная страница 404."""
     return render(request, 'pages/404.html', status=404)
 
 
 def csrf_failure(request, reason=''):
-    """Кастомная страница 403 CSRF."""
     return render(request, 'pages/403csrf.html', status=403)
 
 
 def server_error(request):
-    """Кастомная страница 500."""
     return render(request, 'pages/500.html', status=500)
